f3de1b3da772bf7390189cb99adcfacdfe3055ee
João Pedro Erhardt <joaopedro.erhardt@gmail.com>
2024-06-17 19:29:55 -0300

    add func de criar mapa

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 import java.util.Scanner;
 public class Main {
+    public static void criarMapa() {
+        System.out.println("  1 2 3 4 5 6 7 8");
+        for (int i = 0; i < 8; i++) {
+            System.out.println(i+1 + " ~ ~ ~ ~ ~ ~ ~ ~");
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        criarMapa();
     }
 }
