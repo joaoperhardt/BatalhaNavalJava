94ccd47e714aa6266d50a1f650fffd66f9b3d39d
João Pedro Erhardt <joaopedro.erhardt@gmail.com>
2024-06-20 23:31:51 -0300

    finalizando jogo

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,105 @@
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 public class Main {
-    public static void criarMapa(int mapa[][]) {
+    public static void preencherMatrizes(String mapa[][], String mapaNavios[][]) {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                mapa[i][j] = "~";
+            }
+        }
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                mapaNavios[i][j] = "O";
+            }
+        }
+    }
+    public static void criarMapa(String mapa[][]) {
         System.out.println("  0 1 2 3 4 5 6 7");
         for (int i = 0; i < mapa.length; i++) {
-            System.out.println(i + " ~ ~ ~ ~ ~ ~ ~ ~");
+            System.out.print(i);
+            for (int j = 0; j < mapa.length; j++) {
+                System.out.print(" " + mapa[i][j]);
+            }
+            System.out.println();
         }
     }
 
-    public static void criarNavios(int mapa[][]) {
+    public static void criarNavios(String mapaNavios[][], int navios) {
         Random gerador = new Random();
 
-        int coordenadaX = 0;
         int coordenadaY = 0;
+        int coordenadaX = 0;
 
-        for (int i = 0; i < 10; i++) {
-            coordenadaX = gerador.nextInt(8);
+        for (int i = 0; i < navios; i++) {
             coordenadaY = gerador.nextInt(8);
+            coordenadaX = gerador.nextInt(8);
 
-            if (mapa[coordenadaX][coordenadaY] == 1) {
+            if (mapaNavios[coordenadaY][coordenadaX] == "X") {
                 i--;
                 continue;
             }
-            mapa[coordenadaX][coordenadaY] = 1;
+            mapaNavios[coordenadaY][coordenadaX] = "X";
         }
     }
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int mapa[][] = new int [8][8];
+        String mapa[][] = new String [8][8];
+        String mapaNavios[][] = new String[8][8];
+        int navios = 5, tentativas = 5;
 
-        criarMapa(mapa);
-        criarNavios(mapa);
+        preencherMatrizes(mapa, mapaNavios);
+
+        criarNavios(mapaNavios, navios);
+
+        while (true) {
+            try {
+                if (tentativas == 0) {
+                    break;
+                }
+                criarMapa(mapa);
+
+                scan.useDelimiter("[\\s,]+");
+                System.out.println("Digite a coordenada:");
+                int coordenadaY = scan.nextInt();
+                int coordenadaX = scan.nextInt();
+
+
+                if (mapa[coordenadaY][coordenadaX] == "O" || mapa[coordenadaY][coordenadaX] == "X") {
+                    System.out.println("A coordenada já foi jogada.");
+                    continue;
+                }
+                else if (mapaNavios[coordenadaY][coordenadaX] == "X") {
+                    mapa[coordenadaY][coordenadaX] = mapaNavios[coordenadaY][coordenadaX];
+                    navios--;
+                }
+                else if (mapaNavios[coordenadaY][coordenadaX] == "O") {
+                    mapa[coordenadaY][coordenadaX] = mapaNavios[coordenadaY][coordenadaX];
+                }
+
+                if (navios == 0) {
+                    break;
+                }
+                tentativas--;
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Digite uma coordenada válida.");
+            } catch (InputMismatchException i) {
+                System.out.println("Digite um número para as coordenadas.");
+            }
+        }
+
+        if (navios == 0) {
+            criarMapa(mapa);
+            System.out.println("Você ganhou!");
+        }
+        else {
+            criarMapa(mapa);
+            System.out.println("Suas tentativas acabaram.");
+        }
+
+        scan.close();
     }
 }
